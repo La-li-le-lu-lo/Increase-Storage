e4895f18c04d7ea54c690924ec8f4b3eab34ca27
La-li-le-lu-lo <126132858+La-li-le-lu-lo@users.noreply.github.com>
2024-08-29 12:45:08 -0400

    Add files via upload

---
+++ disk_expand.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Script to expand storage on linux machines
+
+
+echo "Check Storage:"
+df -h
+echo "-----------------------------"
+echo "Which logical volume "
+read lv
+echo "-----------------------------"
+echo "Check Volume Group:"
+vgs
+echo "-----------------------------"
+echo "Which vg"
+read vg
+if [ $vg ]; then
+  lvs $vg
+else
+  echo "No Input Selected please make a selection or ctrl c to quit" 
+fi
+#sleep 10
+echo "-----------------------------"
+echo "How much space to expand by in G"
+read space
+lvextend -L +$space /dev/$vg/$lv -r
+echo "Lets Verify the disk has increased"
+df -h | grep $lv
